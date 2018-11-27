6494e20a4c9d25e15534450a30c811a8b44c7dc1
Skylar <skylarhimeno@gmail.com>
2018-11-27 08:16:20 -0700

    First Frame was created and super was put in as it should always be. this. was established

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,6 +1,5 @@
 package gui.controller;
 
-import gui
 public class GUIController
 {
 	
--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -1,6 +1,29 @@
 package gui.view;
 
-public class FirstFrame
-{
+import javax.swing.JFrame;
+import gui.controller.GUIController;
 
+public class FirstFrame extends JFrame
+{
+	private GUIController appController;
+	private SamplePanel appPanel;
+	
+	public FirstFrame(GUIController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		this.appPanel = new SamplePanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1000, 500);
+		this.setTitle("First GUI App");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
